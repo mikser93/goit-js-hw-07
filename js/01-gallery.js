8a6514ae914a5e11bb99586edbbca4d8909c86c2
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-08-10 20:38:42 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgCollection = document.querySelector(".gallery");
+const imgMarkup = galleryItems
+  .map(
+    (item) =>
+      `<div class ="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    data-source="${item.original}" alt="${item.description}"/>
+    </a>
+    </div>`
+  )
+  .join("");
+
+imgCollection.insertAdjacentHTML("afterbegin", imgMarkup);
+
+imgCollection.addEventListener("click", clickedImg);
+
+function clickedImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImg = event.target.dataset.source;
+  const modalBox = basicLightbox.create(
+    `
+    <img src="${selectedImg}" width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", keyboardEsc);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", keyboardEsc);
+      },
+    }
+  );
+
+  modalBox.show();
+
+  function keyboardEsc(event) {
+    if (event.code === "Escape") {
+      modalBox.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const photoCollection = document.querySelector(".gallery");
+const imgMarkup = galleryItems
+  .map(
+    (item) =>
+      `<li>
+      <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+      </li>`
+  )
+  .join("");
+
+  photoCollection.insertAdjacentHTML("afterbegin", imgMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
